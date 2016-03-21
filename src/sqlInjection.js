ee58b60c0aa03ae1d44af5722b52911c5fd62cd1
George Mauer <gmauer@gmail.com>
2016-03-20 21:02:46 -0500

    adjustments for sqlinjection i dunno i forgot what these did

--- src/sqlInjection.js
+++ src/sqlInjection.js
@@ -1,23 +1,8 @@
+'use strict'
+
 const co = require('co')
-const sqlite3 = require('co-sqlite3');
-
-const create = () => {
-  // db.get("SELECT name FROM sqlite_master WHERE type='table'", []).then(res => {
-  //   if
-  // })
-}
-function callbacksToResRej(resolve, reject) { return function callbacksToResRej(err) {
-  if(err) return reject(err)
-  resolve([...arguments].slice(1))
-} }
-function promisify(obj, methodName) { return function() {
-    var args = [...arguments]
-    return new Promise((resolve, reject) => {
-      obj[methodName].apply(obj, args.concat([callbacksToResRej(resolve, reject)]))
-    })
-}}
-
-
+const sqlite3 = require('co-sqlite3')
+const uuid = require('node-uuid')
 
 const runIt = (dbLocation) =>  {
   dbLocation || (dbLocation = `${__dirname}/../data/db.db`)
@@ -25,12 +10,31 @@ const runIt = (dbLocation) =>  {
   return co(function*(){
     const db = yield sqlite3(dbLocation);
     try {
-      const tableExists = yield db.get("SELECT 1 yes FROM sqlite_master WHERE type='table' AND name LIKE 'lorem' LIMIT 1", [])
-      if(!tableExists || !tableExists[0] || !tableExists[0].yes)
-          yield promisify(db, 'run')("CREATE TABLE lorem (info TEXT)", [])
+      const tableExists = yield db.get(`SELECT 1 yes FROM sqlite_master WHERE type='table' AND name LIKE 'products' LIMIT 1`, [])
+      if(tableExists && tableExists[0]  && tableExists[0].yes)
+        return
+      yield db.run("CREATE TABLE producs (id UUID primary key, name TEXT)", [])
+      // var stmt = yield db.prepare('INSERT INTO testtable(id) VALUES( ? )');
+      // for(var i =0 ; i < 100 ; i++){
+      //   yield stmt.run(i);
+      // }
+      //
+      // stmt.finalize();
+      //
+      // var row = yield db.get('SELECT * FROM testtable WHERE id < ? ORDER BY ID DESC ' ,[50]);
+      // console.log(row); // {id: 49}
+      //
+      // var rows = yield db.all('SELECT * FROM testtable');
+      // console.log(rows.length);
 
-      const res = yield promisify(db, 'get')("SELECT name FROM sqlite_master WHERE type='table'", [])
-      console.log(res)
+      const stmt = yield db.prepare("INSERT INTO products VALUES (?, ?)", []);
+      for(let i = 0; i < 10; i+=1)
+        yield stmt.run(uuid.v1(), `Product ${1}`)
+      console.log('ok?')
+      stmt.finalize();
+
+      const rows = yield db.each("SELECT * FROM products")
+      console.log(rows.length)
     } catch(err) {
       console.log(err)
       throw err
@@ -41,13 +45,3 @@ const runIt = (dbLocation) =>  {
 }
 
 module.exports = runIt
-    // var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-    // for (var i = 0; i < 10; i++)
-    //     stmt.run("Ipsum " + i);
-    //
-    // stmt.finalize();
-    //
-    // db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-    //     console.log(row.id + ": " + row.info);
-    // });
-  // db.close();
